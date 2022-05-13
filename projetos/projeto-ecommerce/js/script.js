ddd7768e4825e5c4048ce60c755f8f1da31c2e18
TheusBarboza <theusbdesigner@gmail.com>
2022-05-13 17:52:51 -0300

    Atualização no menu mobile

--- projetos/projeto-ecommerce/js/script.js
+++ projetos/projeto-ecommerce/js/script.js
@@ -1,15 +1,19 @@
 $('.banner').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  });
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
 
 const btnMobile = document.getElementById('btnMobile');
 
-function toggleMenu(){
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
   const nav = document.getElementById('nav');
   nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
